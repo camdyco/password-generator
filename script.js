874019fb960e5efc411827d5d587a1acce020180
Cameron Cook <cameroncook@Camerons-MBP.lan>
2023-03-02 23:03:31 -0500

    added a helper function

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 // Define character sets for password requirements
-var lowerCase = ['abcdefghijklmnopqrstuvwxyz'];
-var upperCase = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
-var numberChars = ['0123456789'];
-var specialCharacters = '!@#$%^&*()_+~`|}{[]:;?><,./-='
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberChars = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = ['!','@','#','$','%','^','&','*','(',')','_','+','~','|','}','{','[',']',':',';','?','>','<',',','.','/','-','='];
+var allCharacters = []
+var storePassword = 
 
 document.querySelector("#generate").onclick = generatePassword;
 
-function generatePassword () {
+function generatePassword() {
 
   // prompt the user for password criteria
   var passwordLength = parseInt(prompt("how many characters is your password? (must be between 8 and 128"));
@@ -21,14 +23,39 @@ function generatePassword () {
   }
 
   var includeNumbers = confirm("Would you like to include numbers?");
-  var includeSpecialChars = confirm("Would you like to include special characters?");
+  var includeSpecialCharacters = confirm("Would you like to include special characters?");
   var includeUpperCase = confirm("Would you like to include upper case letters?");
   var includeLowerCase = confirm("Would you like to include lower case letters?");
 
-  // if (includeNumbers == true){
-  //  } else if (includeNumbers == false){
-  // }
-  // if (includeSpecialChars == true){ 
-  // } else if (includeSpecialChars == false){
-  // }
+  // step 1: check each include variable and add from corresponding character list if true
+  if (includeNumbers == true){
+    allCharacters = allCharacters.concat(numberChars);
+     }
+     console.log(allCharacters);
+  if (includeSpecialCharacters == true){
+    allCharacters = allCharacters.concat(specialCharacters);
+    }
+       console.log(allCharacters);
+  if (includeUpperCase == true){
+    allCharacters = allCharacters.concat(upperCase);
+  }
+    console.log(allCharacters);
+  if (includeLowerCase == true){
+    allCharacters = allCharacters.concat(lowerCase);
+  }
+  console.log(allCharacters);
+}
+  // step 2: select one random character for each prompt they said yes to
+
+
+
+  // step 3: 
+
+// helper function to generate a random character
+function generateRandomCharacter(placeholder)
+{
+  let placeholderLength = placeholder.length;
+  let randomGeneratedNumber = Math.floor((Math.randon() * placeholderLength) -1);
+  let randomCharacter = placeholder[randomGeneratedNumber];
+  return randomCharacter;
 }
